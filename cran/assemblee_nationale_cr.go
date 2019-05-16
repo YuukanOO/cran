91cc2f757c69cf1b113b362c11fd987b0e86994b
Julien LEICHER <j.leicher@biolog-id.com>
2019-05-16 16:30:45 +0200

    wip: Start working on a tree like structure

--- cran/assemblee_nationale_cr.go
+++ cran/assemblee_nationale_cr.go
@@ -1,5 +1,53 @@
 package cran
 
+type Root struct {
+	children []Node
+}
+
+func newRoot() *Root {
+	return &Root{
+		children: make([]Node, 0),
+	}
+}
+
+func (r *Root) Append(n Node) { r.children = append(r.children, n) }
+func (r *Root) Children() []Node { return r.children }
+
+type Node interface {
+	Append(n Node)
+	Children() []Node
+}
+
+type SectionNode struct{
+	Title string
+	children []Node
+}
+
+func (r *SectionNode) Append(n Node) { r.children = append(r.children, n) }
+func (r *SectionNode) Children() []Node { return r.children }
+
+func newSection(title string) *SectionNode {
+	return &SectionNode{
+		Title: title,
+		children: make([]Node, 0),
+	}
+}
+
+type InterventionNode struct {
+	SpeakerID string
+	Sentence string
+}
+
+func (r *InterventionNode) Append(n Node) { }
+func (r *InterventionNode) Children() []Node { return nil }
+
+func newIntervention(speaker, sentence string) *InterventionNode {
+	return &InterventionNode{
+		SpeakerID: speaker,
+		Sentence: sentence,
+	}
+}
+
 type assembleNationaleCRProvider struct{}
 
 func (p *assembleNationaleCRProvider) Fetch(URL string, callback ProviderCallback) {
--- cran/assemblee_nationale_cr_test.go
+++ cran/assemblee_nationale_cr_test.go
@@ -26,56 +26,49 @@ func newTestServer() *httptest.Server {
 		<h1 class="seance">Deuxième séance du lundi 13 mai 2019</h1>
 		<div id="somjo"></div>
 		<div class="ouverture_seance">
-			<h5 class="presidence"><a name="P1714018" id="P1714018"><span></span></a>Présidence de Mme&nbsp;Annie Genevard<br><br>vice-présidente</h5>
+			<h5 class="presidence">
+				<a name="P1714018" id="P1714018"><span></span></a>Présidence de Mme&nbsp;Annie Genevard<br><br>vice-présidente
+			</h5>
 			<p>
-				<a name="P1714019" id="P1714019">
-						<!--ANCRE-->
-				</a><b><a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA605991">Mme la présidente</a></b>. La séance est ouverte.</p>
+				<b>
+					<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA605991">Mme la présidente</a>
+				</b>. La séance est ouverte.
+			</p>
 			<p> <i>(La séance est ouverte à vingt et une heures trente.)</i></p>
 		</div>
 
-		<div class="Point"><h5 class="numencad">1</h5><h2 class="titre1">Transformation de la fonction publique<i></i></h2></div>
+		<div class="Point">
+			<h5 class="numencad">1</h5>
+			<h2 class="titre1">Transformation de la fonction publique<i></i></h2>
+		</div>
 		
 		<div class="Point">
-		<h2 class="titre2">Discussion générale</h2>
-		<div class="intervention">
+			<h2 class="titre2">Discussion générale</h2>
+			<div class="intervention">
+				<p>
+					<b>
+						<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA1874">M. le président</a>
+					</b>.  La parole est à Mme&nbsp;Frédérique Dumas.
+				</p>
+			</div>
+
+			<div class="Point">
+				<h2 class="titre99">
+					<i>Suspension et reprise de la séance</i>
+				</h2>
+				<p>
+					<b>
+						<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA1874">M. le président</a>
+					</b>.  La séance est suspendue.
+				</p>
+			</div>
 			<p>
-				<a name="P1710028" id="P1710028">
-					<!--ANCRE-->
-				</a>
 				<b>
-					<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA1874">M. le président</a>
-				</b>.  La parole est à Mme&nbsp;Frédérique Dumas.
+					<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA718868">M. Michel Larive</a>
+				</b>.  Avec toutes les citations énumérées depuis ce matin, si les œuvres de Victor Hugo étaient toujours soumises à droits d’auteur, et si –&nbsp;comme il l’aurait souhaité&nbsp;– la manne financière participait au financement de la création d’artistes vivants, nous aurions ce soir suffisamment de subsides pour aider un grand nombre de créateurs français&nbsp;! 
+				<i>(Sourires.)</i>
 			</p>
 		</div>
-		<a name="P1709791" id="P1709791">
-			<!--ANCRE-->
-		</a>
-		<div class="Point">
-			<h2 class="titre99">
-				<i>Suspension et reprise de la séance</i>
-			</h2>
-			<p>
-				<a name="P1709792" id="P1709792">
-					<!--ANCRE-->
-				</a>
-				<b>
-					<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA1874">M. le président</a>
-				</b>.  La séance est suspendue.
-			</p>
-		</div>
-		<p>
-			<a name="P1709796" id="P1709796">
-				<!--ANCRE-->
-			</a>
-			<b>
-				<a href="http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA718868">M. Michel Larive</a>
-			</b>.  Avec toutes les citations énumérées depuis ce matin, si les œuvres de Victor Hugo étaient toujours soumises à droits d’auteur, et si –&nbsp;comme il l’aurait souhaité&nbsp;– la manne financière participait au financement de la création d’artistes vivants, nous aurions ce soir suffisamment de subsides pour aider un grand nombre de créateurs français&nbsp;! 
-			<i>(Sourires.)</i>
-			<br>
-				<br>Le 15&nbsp;avril&nbsp;2019, un incendie de grande ampleur a ravagé la cathédrale Notre-Dame de Paris. Ce chef-d’œuvre de l’architecture du Moyen-Âge, inscrit au patrimoine mondial de l’UNESCO au titre de divers critères –&nbsp;dont celui du génie créateur humain&nbsp;– a subi d’irréversibles dommages. Les images de l’effondrement de la flèche, retransmises en direct sur les écrans de télévision, ont provoqué une onde de choc ressentie bien au-delà de nos frontières.
-																																				</p>
-																																			</div>
 	</div>
 </div>
 </body>
@@ -93,4 +86,22 @@ func TestAssembleeNationaleCRParsing(t *testing.T) {
 	p.Fetch(ts.URL, func (report *Report, err error) {
 		
 	})
+}
+
+func TestNodes(t *testing.T) {
+	r := newRoot()
+	s1 := newSection("One section")
+	s2 := newSection("Another one")
+	s1.Append(s2)
+	r.Append(s1)
+
+	if len(r.Children()) != 1 {
+		t.Error("Should be 1")
+	}
+
+	s := r.Children()[0].(*SectionNode)
+
+	if s.Title != "One section" {
+		t.Error("Title should be One section")
+	}
 }
