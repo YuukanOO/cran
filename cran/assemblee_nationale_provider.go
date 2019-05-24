525118954155b06f579401ea4627ef436d79e973
YuukanOO <jleicher@gmail.com>
2019-05-24 21:29:58 +0200

    fix: Now use async visit!

--- cran/assemblee_nationale_provider.go
+++ cran/assemblee_nationale_provider.go
@@ -18,12 +18,26 @@ func (p *assembleeNationale) Name() string {
 }
 
 func (p *assembleeNationale) Fetch(URL string, callback ProviderCallback) {
+	speakers := make(map[string]*Speaker)
 	report := newReport(URL)
 
-	c := colly.NewCollector()
+	c := colly.NewCollector(colly.Async(true))
 
-	c.OnHTML("h1", func(e *colly.HTMLElement) {
+	c.OnHTML(".SYCERON h1", func(e *colly.HTMLElement) {
 		report.Title = e.Text
+	})
+
+	c.OnHTML("#deputes-fiche", func(e *colly.HTMLElement) {
+		speaker := speakers[e.Request.URL.String()]
+
+		title := e.DOM.Find("h1")
+		img := e.DOM.Find(".deputes-image img")
+
+		speaker.Name = title.Text()
+		speaker.URL = e.Request.URL.String()
+		speaker.PictureURL = e.Request.AbsoluteURL(img.AttrOr("src", ""))
+		speaker.Location = strings.TrimSpace(title.Next().Text())
+		speaker.Side = strings.TrimSpace(img.Parent().Parent().Children().Last().Text())
 	})
 
 	c.OnHTML(".Point", func(ele *colly.HTMLElement) {
@@ -37,8 +51,6 @@ func (p *assembleeNationale) Fetch(URL string, callback ProviderCallback) {
 		section := report.addSection(sectionTitle)
 
 		ele.DOM.Find("p").Each(func(i int, e *goquery.Selection) {
-			d := c.Clone()
-
 			// Find the author
 			author := e.Find("a[href]")
 
@@ -57,29 +69,24 @@ func (p *assembleeNationale) Fetch(URL string, callback ProviderCallback) {
 
 			section.addIntervention(authorName, sentence)
 
-			// Upon profile visit success, keep some information on it
-			d.OnHTML("html", func(profileEle *colly.HTMLElement) {
-				title := profileEle.DOM.Find("h1")
-				img := profileEle.DOM.Find(".deputes-image img")
+			if authorLink, authorLinkExists := author.Attr("href"); authorLinkExists {
+				if _, speakerExists := speakers[authorLink]; !speakerExists {
+					speakers[authorLink] = &Speaker{
+						ID: authorName,
+					}
 
-				report.addSpeaker(authorName,
-					title.Text(),
-					profileEle.Request.URL.String(),
-					profileEle.Request.AbsoluteURL(img.AttrOr("src", "")),
-					title.Next().Text(),
-					img.Parent().Parent().Last().Text())
-			})
-
-			// And visits its profile
-			if authorLink, exists := author.Attr("href"); exists {
-				d.Visit(authorLink)
+					ele.Request.Visit(authorLink)
+				}
 			}
 		})
 	})
 
-	c.OnScraped(func(r *colly.Response) {
-		callback(report, nil)
-	})
-
 	c.Visit(URL)
+	c.Wait()
+
+	for _, speaker := range speakers {
+		report.Speakers[speaker.ID] = speaker
+	}
+
+	callback(report, nil)
 }
